134d41a6971075bc146868e2ba3bf023fd463614
raulrm <raulrm@elpuig.xeill.net>
2021-10-13 19:42:59 +0200

    Initial commit

---
+++ src/com/company/Llapis.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Llapis {
+    int colores;
+    int grueso;
+    String lapiz;
+    List<String> caixa = new ArrayList<String>(Integer.parseInt(lapiz));
+    List<String> caixa2 = new ArrayList<String>(Integer.parseInt(lapiz));
+}
